5a1f4ac4f4b64cb16259e464eb05f4a6ca9410f8
Ashish Prajapati <ashish.mscs@gmail.com>
2017-10-28 03:39:41 +0530

    fix broken tag issue

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-hexo.extend.filter.register('after_post_render', function (data) {
-    var excerptLength = hexo.config.excerpt_length || 300;
-    var excerpt = data.content.substr(0, excerptLength);
-    data.excerpt = excerpt;
-    return data;
-});
+(function(){
+    const sanitize = function (excerpt) {
+        let lGt = excerpt.lastIndexOf('>');
+        let lLt = excerpt.lastIndexOf('<');
+        if (lGt > lLt) {
+            return excerpt;
+        }
+        else {
+            return excerpt.substr(0, lLt - 1);
+        }
+    }
+    
+    hexo.extend.filter.register('after_post_render', function (data) {
+        var excerptLength = hexo.config.excerpt_length || 300;
+        var excerpt = data.content.substr(0, excerptLength);
+        var cleanExcerpt = sanitize(excerpt);
+        data.excerpt = cleanExcerpt;
+        return data;
+    });
+})();
