e393cac28e93770c89d941ce49336e1e477ed63e
Ashish Prajapati <ashish.mscs@gmail.com>
2017-10-28 19:51:23 +0530

    enhanced excerpt extraction from post

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
+const htmlToText = require('html-to-text');
 (function(){
-    const sanitize = function (excerpt) {
-        let lGt = excerpt.lastIndexOf('>');
-        let lLt = excerpt.lastIndexOf('<');
-        if (lGt > lLt) {
-            return excerpt;
-        }
-        else {
-            return excerpt.substr(0, lLt - 1);
-        }
+    const sanitize = function (post) {
+        const content = htmlToText.fromString(
+            post,
+            {
+                ignoreImage: true,
+                ignoreHref: true,
+                wordwrap: true,
+                uppercaseHeadings: false
+            }
+        );
+        return content;
     }
-    
+
     hexo.extend.filter.register('after_post_render', function (data) {
-        var excerptLength = hexo.config.excerpt_length || 300;
-        var excerpt = data.content.substr(0, excerptLength);
-        var cleanExcerpt = sanitize(excerpt);
-        data.excerpt = cleanExcerpt;
+        const excerptLength = hexo.config.excerpt_length || 300;
+        const post = sanitize(data.content);
+        const excerpt = post.substr(0, excerptLength);
+        data.excerpt = excerpt;
         return data;
     });
 })();
